ac6a2781565d423b8b333c1af20be4989b0158ae
Rohi P.J Apeswini Awinbe-Yisazuah <100289361+R0hi77@users.noreply.github.com>
2023-08-12 01:42:32 +0000

    Add files via upload

---
+++ automatic_water_dispense_code/automatic_water_dispense_code.ino
@@ -0,0 +1,42 @@
+int relayPin=5;
+int triggerPin=10;
+int echoPin=12;
+
+long  readDistance(int triggerPin, int echoPin)
+  {
+    pinMode(triggerPin,OUTPUT);
+    
+    
+    digitalWrite(triggerPin,LOW);
+    delayMicroseconds(2);
+    digitalWrite(triggerPin,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(triggerPin,LOW);
+
+    pinMode(echoPin,INPUT);
+    return pulseIn(echoPin,HIGH);
+  }
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(relayPin,OUTPUT);
+  Serial.begin(9600);
+
+ 
+  
+
+}
+
+void loop() {
+  long distance;
+  distance=0.0175*readDistance(triggerPin, echoPin);
+  Serial.println(distance);
+  delay(20);
+
+   if(distance<10)
+  {digitalWrite(relayPin,HIGH);}
+  else{
+    digitalWrite(relayPin,LOW);}
+  }
+  
+
+
